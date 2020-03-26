46516369a1229c50edb559822d5dc27f22ebafa6
makarimachmad <makarimachmad@gmail.com>
2020-03-26 13:18:31 +0700

    benerin eventlog

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->get('/key', function() { 
-    return \Illuminate\Support\Str::random(32); 
-});
+#$router->get('/key', function() { 
+#    return \Illuminate\Support\Str::random(32); 
+#});
 
 $router->post('/Webhook', 'Webhook');
